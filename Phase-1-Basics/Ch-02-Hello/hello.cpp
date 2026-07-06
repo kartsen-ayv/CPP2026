723de4b39fd09b672b534046e22254fe91f9d70d
Kartsen <karen.ayv12599@gmail.com>
2026-07-06 21:49:11 +0300

    feat(ch02): add hello world examples using cout, printf, and C++23 print

---
+++ Phase-1-Basics/Ch-02-Hello/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <print> // run only with "-lstdc++exp" key
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl; // default output
+    printf("Hi World!\n"); // c-style output
+    println("Hi there!"); // C++23 format output
+
+    return 0;
+}
