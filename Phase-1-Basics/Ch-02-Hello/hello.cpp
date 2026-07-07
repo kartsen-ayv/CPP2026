ad27e0b81bd55bbeebb589657f30fefe289b2b11
Kartsen <karen.ayv12599@gmail.com>
2026-07-07 20:57:09 +0300

    feat(ch02): comments have been updated and specific functions are being pulled up instead of the whole namespace

--- Phase-1-Basics/Ch-02-Hello/hello.cpp
+++ Phase-1-Basics/Ch-02-Hello/hello.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstdio>
 #include <print> // run only with "-lstdc++exp" key
-using namespace std;
+using std::cout, std::endl, std::println;
 
 int main()
 {
     cout << "Hello world!" << endl; // default output
-    printf("Hi World!\n"); // c-style output
-    println("Hi there!"); // C++23 format output
+    printf("Hi World!\n");          // c-style output
+    println("Hi there!");           // C++23 format output
 
     return 0;
 }
